d2bf1b4c1005df003f25ac40073170e9acbb6485
unknown <gimperro.business.contact@gmail.com>
2024-07-17 20:32:54 +0200

    init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=SECRET_KEY,   # Change to random!!!
+        SECRET_KEY=os.environ.get("SECRET"),   # Change to random!!!
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
