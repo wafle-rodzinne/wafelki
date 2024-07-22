092937305b61d251e9cbabd93890efa58901aee0
unknown <gimperro.business.contact@gmail.com>
2024-07-22 19:34:08 +0200

    part of frontend

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ from flask import (Flask, redirect, url_for)
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',#os.environ.get("SECRET_KEY"),
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
